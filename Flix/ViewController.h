b47a4e0b0a859740744e82791da973296b11fff9
Felianne Teng <felianneteng@fb.com>
2021-06-23 08:52:10 -0700

    Initial Commit

---
+++ Flix/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Felianne Teng on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by Felianne Teng on 6/23/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
